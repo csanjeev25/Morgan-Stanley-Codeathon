2dfea61c5403382e6233e40af134b15af39e2c16
Sanjeevkumar Chintakindi <sanjeevkumarchintakindi@gmail.com>
2017-11-12 17:07:18 +0530

    Update maximumProfit.py

--- maximumProfit.py
+++ maximumProfit.py
@@ -1,26 +1,22 @@
 #!/bin/python3
 
 import sys
-from itertools import cycle
 
-def expectedAmount(a):
+def maximizeProfit(a, b, m, k):
     # Complete this function
-    count = list()
+    bitcoinDollar = m*k
+    c = [];
+    c.append(bitcoinDollar)
     for i in range(n):
-        count.append(0)
-    sum = 0
-    pool = cycle(a)
-    poo1_1 = cycle(b)
-    count_1 = 2
-    for i in pool:
-        count_2 = 2
-        
-        
-   
+        d = m * b[i] * a[i];
+        c.append(d);
+    e = max(c);
+    return e
 
 if __name__ == "__main__":
-    t = int(input().strip())
-    for a0 in range(t):
-        n = int(input().strip())
-        a = list(map(int, input().strip().split(' ')))
-        expectedAmount(a)
+    n, m, k = input().strip().split(' ')
+    n, m, k = [int(n), int(m), int(k)]
+    a = list(map(int, input().strip().split(' ')))
+    b = list(map(int, input().strip().split(' ')))
+    result = maximizeProfit(a, b, m, k)
+    print(result)
