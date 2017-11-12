66f9b537a8dbf14016c56e5aafa48c6127acefab
csanjeev25 <sanjeevkumarchintakindi@gmail.com>
2017-11-12 16:16:29 +0530

    Adiosgit add .

---
+++ maximumProfit.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import sys
+from itertools import cycle
+
+def expectedAmount(a):
+    # Complete this function
+    count = list()
+    for i in range(n):
+        count.append(0)
+    sum = 0
+    pool = cycle(a)
+    poo1_1 = cycle(b)
+    count_1 = 2
+    for i in pool:
+        count_2 = 2
+        
+        
+   
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        n = int(input().strip())
+        a = list(map(int, input().strip().split(' ')))
+        expectedAmount(a)
